8be70eb60094351fa1cec13c19279b976d970a3a
Rui Fernandes <ruifernandes155@gmail.com>
2025-01-21 15:47:41 -0500

    Divisors Practice

--- ListLessThanTen.py
+++ ListLessThanTen.py
@@ -1,6 +1,6 @@
 # List Less Than Ten  
 # list numbers elements if conditional
-# Exercise 3 (and Solution)
+# Exercise 3
 # Take a list, say for example this one:
 
 #   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
