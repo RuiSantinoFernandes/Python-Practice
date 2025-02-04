db52114f66a01b3dba0269f208ade0cd7ebd8d86
Rui Fernandes <ruifernandes155@gmail.com>
2025-02-03 20:01:13 -0500

    String Lists practice

--- StringLists.py
+++ StringLists.py
@@ -1,6 +1,6 @@
 # String Lists  
 # strings lists index
-# Exercise 6 (and Solution)
+# Exercise 6 (Solution)
 # Ask the user for a string and print out whether this string is 
 # a palindrome or not. (A palindrome is a string that reads 
 # the same forwards and backwards.)
